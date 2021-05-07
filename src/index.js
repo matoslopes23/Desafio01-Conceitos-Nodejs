6295511570ee9b37a2660403961e5f5c652831b8
Lucas Weslen <matoslopes23>
2021-05-07 14:11:28 -0400

    Atualização dos metodos http

--- src/index.js
+++ src/index.js
@@ -1,41 +1,122 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  //Buscar no array users se já existe um username cadastrado
+  const user = users.find(user=> user.username===username);
+  // se  user for falso retorna um status 400 com a mensagem de que o usuário não foi encntrado
+  if(!user){
+    return response.status(404).json({error:"User not found"})
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+  // Buscar no array users se já existe um username cadastrado
+  //  usando o método some() que irá retornar um valor true ou false 
+  const userExists = users.find(
+    (user) => user.username===username 
+  );
+  // Se já existir um username cadastrado,
+  //  o sistema retornara um erro 400 informando que o username já existe
+  if(userExists){
+    return response.status(400).json({error: "username already exists"});
+  }
+  
+  const user = {
+    id:uuidv4(),
+    name,
+    username,
+    todos:[]
+  }
+  // Add um novo usuário
+  users.push(user);
+
+  return response.status(201).json(user);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  // retorna os todos
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {title, deadline} = request.body;
+  const {user} = request;
+
+  // criação do objeto 
+  const totoIncrement = {
+    id:uuidv4(),
+    title,
+    done: false,
+    deadline:new Date(deadline) ,
+    created_at: new Date()
+  }
+  // Add um elemento no array
+  user.todos.push(totoIncrement);
+
+  return response.status(201).json(totoIncrement);
+
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params;
+  const {title, deadline} = request.body;
+  const {user} = request;
+  // pega o id do todo buscando no array para ver se existe
+  const todo = user.todos.find(todo => todo.id===id)
+  // se todo for false ou seja se não retornar nada, exibir o error "tod not found"
+  if(!todo){
+    return response.status(404).json({error:"Todo not found"})
+  }
+  // recebe as atualizações
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo)
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params;
+  const {user} = request;
+
+  // se todo for false ou seja se não retornar nada, exibir o error "tod not found"
+  const todo = user.todos.find(todo => todo.id===id)
+  if(!todo){
+    return response.status(404).json({error:"Todo not found"})
+  }
+  todo.done = true;
+  return response.json(todo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  const {id} = request.params;
+  // pega o id do todo e o index 
+  const todoIndex = user.todos.findIndex(todo => todo.id===id)
+  if(todoIndex===-1){
+    return response.status(404).json({error:"Todo not found"})
+  }
+  user.todos.splice(todoIndex, 1);
+  return response.status(204).json();
+
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 const app = require('./');
 
-app.listen(3333);
+app.listen(3333,()=> console.log("Server is Runner"));
